067f6cad9f25fb708fbe1fa5a04e8f4d1c4fc263
purplechonk <wenqi.liu.2023@smu.edu.sg>
2024-10-10 18:37:00 +0800

    debug homepage

--- js/script.js
+++ js/script.js
@@ -56,3 +56,14 @@ if (videoModal !== null) {
     video.setAttribute('src', videoSrc);
   });
 }
+
+// Initialize ScrollMagic Controller
+var controller = new ScrollMagic.Controller();
+
+// Create Scene
+new ScrollMagic.Scene({
+  triggerElement: "#pane1", // Start effect when this element is in view
+  duration: "100%"          // Continue effect throughout the height of pane1
+})
+.setPin("#pane1")            // Pin pane1 while scrolling through its height
+.addTo(controller);          // Add this scene to the controller
